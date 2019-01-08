5065fbcaae0404f3ae95c05b8615904a84913a5f
David Rose <doppler@gmail.com>
2019-01-08 07:19:01 -0500

    cleanup

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 const SerialPort = require("serialport");
-const events = require("events");
-const LoopParser = require("./lib/loop-parser");
-const parseLoopPacket = require("./lib/parseLoopPacket");
+const VantageLoopPacketParser = require("./lib/vantage-loop-packet-parser");
+const jsonFromVantageLoopPacket = require("./lib/json-from-vantage-loop-packet");
 
 const config = require("./config");
-
-const WeatherStation = new events.EventEmitter();
 
 const port = new SerialPort(config.PORT, { baudRate: 19200 }, err => {
   if (err) return console.error("Error:", err.message);
 });
 
-const parser = port.pipe(new LoopParser());
+const parser = port.pipe(new VantageLoopPacketParser());
 
 port.on("open", () => {
-  console.log(`Opened port ${config.PORT}`);
+  const requestLoop = () => port.write("LOOP 1\n");
   // "wakes up" console
   port.write("\n", err => {
     if (err) {
@@ -26,32 +23,15 @@ port.on("open", () => {
     // After recieving "wake up", console replies, and our parser transforms
     // the reply to "ACK". We can now send the "LOOP 1" command.
     if (data.toString() === "ACK") {
-      port.write("LOOP 1\n");
-      return;
+      return requestLoop();
     }
-    const parsedData = parseLoopPacket(data);
-    const dataSubset = config.DESIRED_FIELDS.reduce(
-      (newObj, property) => ((newObj[property] = parsedData[property]), newObj),
-      {}
+    const weatherStationData = jsonFromVantageLoopPacket(data.slice(1));
+    process.stdout.write(
+      JSON.stringify({ _id: new Date(), ...weatherStationData }) + "\n"
     );
-    WeatherStation.emit("loop", { _id: new Date(), ...dataSubset });
 
-    const loopRequestTimeout = setTimeout(() => {
-      port.write("LOOP 1\n");
+    setTimeout(() => {
+      requestLoop();
     }, 2000);
   });
-});
-
-const PouchDB = require("pouchdb");
-const DB = new PouchDB("http://10.101.25.123:5984/wsd");
-
-const saveData = data => {
-  DB.put(data)
-    .then(res => console.log(res))
-    .catch(err => console.error(err.message));
-};
-
-WeatherStation.on("loop", data => {
-  saveData(data);
-  console.log(data);
 });
