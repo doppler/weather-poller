fa6eb2833e93e7fd3c1d619980041b24edea0a45
David Rose <doppler@gmail.com>
2019-01-07 20:53:15 -0500

    clean up code a bit

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ const config = require("./config");
 const WeatherStation = new events.EventEmitter();
 
 const port = new SerialPort(config.PORT, { baudRate: 19200 }, err => {
-  if (err) return console.error;
+  if (err) return console.error("Error:", err.message);
 });
 
 const parser = port.pipe(new LoopParser());
 
 port.on("open", () => {
   console.log(`Opened port ${config.PORT}`);
+  // "wakes up" console
   port.write("\n", err => {
     if (err) {
       return console.error("Error:", err.message);
@@ -22,23 +23,18 @@ port.on("open", () => {
   });
 
   parser.on("data", data => {
-    console.log(new Date(), data, data.length);
+    // After recieving "wake up", console replies, and our parser transforms
+    // the reply to "ACK". We can now send the "LOOP 1" command.
     if (data.toString() === "ACK") {
-      console.debug("Writing LOOP 1");
       port.write("LOOP 1\n");
       return;
     }
-    if (data.toString("utf8", 1, 4) === "LOO" && data.length === 100) {
-      data = data.slice(1);
-      const parsedData = parseLoopPacket(data);
-      const dataSubset = config.DESIRED_FIELDS.reduce(
-        (newObj, property) => (
-          (newObj[property] = parsedData[property]), newObj
-        ),
-        {}
-      );
-      WeatherStation.emit("loop", { _id: new Date(), ...dataSubset });
-    }
+    const parsedData = parseLoopPacket(data);
+    const dataSubset = config.DESIRED_FIELDS.reduce(
+      (newObj, property) => ((newObj[property] = parsedData[property]), newObj),
+      {}
+    );
+    WeatherStation.emit("loop", { _id: new Date(), ...dataSubset });
 
     const loopRequestTimeout = setTimeout(() => {
       port.write("LOOP 1\n");
@@ -46,14 +42,10 @@ port.on("open", () => {
   });
 });
 
-const fs = require("fs");
-const db = fs.createWriteStream("./database.txt");
-
 const PouchDB = require("pouchdb");
 const DB = new PouchDB("http://10.101.25.123:5984/wsd");
 
 const saveData = data => {
-  // db.write(JSON.stringify(data) + "\n");
   DB.put(data)
     .then(res => console.log(res))
     .catch(err => console.error(err.message));
