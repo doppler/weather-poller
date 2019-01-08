397e4bd18360536454c94b4e76025fd0e89416b4
David Rose <doppler@gmail.com>
2019-01-08 14:07:57 -0500

    send data to rethinkdb

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
-const SerialPort = require("serialport");
-const VantageLoopPacketParser = require("./lib/vantage-loop-packet-parser");
-const jsonFromVantageLoopPacket = require("./lib/json-from-vantage-loop-packet");
-const config = require("./config");
-let consoleIsAwake = false;
-let loopRequestInterval;
-const port = new SerialPort(config.PORT, { baudRate: 19200 }, err => {
-  if (err) return console.error("Error:", err.message);
-});
-const parser = port.pipe(new VantageLoopPacketParser());
-const requestLoop = () => {
-  if (port.isOpen && consoleIsAwake) {
-    port.write("LOOP 1\n");
-  }
-};
-const wakeConsole = () => {
-  port.write("\n", err => console.error);
-};
-port.on("open", () => {
-  wakeConsole();
-  parser.on("data", data => {
-    // console.log("data ", data, data.length);
-    // After recieving "wake up", console replies, and our parser transforms
-    // the reply to "ACK". We can now send the "LOOP 1" command.
-    if (data.toString() === "ACK") {
-      consoleIsAwake = true;
-      clearInterval(loopRequestInterval);
-      loopRequestInterval = setInterval(() => {
-        requestLoop();
-      }, 2000);
-      return requestLoop();
-    }
-    process.stdout.write(
-      JSON.stringify(jsonFromVantageLoopPacket(data.slice(1))) + "\n"
-    );
+const program = require("commander");
+const { spawn } = require("child_process");
+const r = require("rethinkdb");
+
+program
+  .version("0.0.1")
+  .option("-h --db-host <host>", "RethinkDB host")
+  .option("-p --db-port <port>", "RethinkDB port")
+  .option("-t --db-table <name>", "RethinkDB table")
+  .option("-l --location <location>", "Weather station location")
+  .parse(process.argv);
+
+if (!program.dbHost || !program.dbTable || !program.location) {
+  console.error(program.outputHelp());
+  process.exit(1);
+}
+r.connect({ host: program.dbHost }).then(conn => {
+  weatherStationFeed.stdout.on("data", data => {
+    const record = {
+      type: "weather",
+      location: program.location,
+      time: new Date(),
+      ...JSON.parse(data)
+    };
+    console.log(record);
+    r.table(program.dbTable)
+      .insert(record)
+      .run(conn, (err, cursor) => {
+        if (err) throw err;
+      });
   });
 });
+
+const weatherStationFeed = spawn("node", ["vantage-poller.js"]);
