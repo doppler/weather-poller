77d61bfa2a9f42a37aa95653b4bf6a4415d58a7e
David Rose <doppler@gmail.com>
2019-01-08 11:49:28 -0500

    workarounds for whatever is plaguing the buffers

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
 const SerialPort = require("serialport");
 const VantageLoopPacketParser = require("./lib/vantage-loop-packet-parser");
 const jsonFromVantageLoopPacket = require("./lib/json-from-vantage-loop-packet");
-
 const config = require("./config");
-
+let consoleIsAwake = false;
+let loopRequestInterval;
 const port = new SerialPort(config.PORT, { baudRate: 19200 }, err => {
   if (err) return console.error("Error:", err.message);
 });
-
 const parser = port.pipe(new VantageLoopPacketParser());
-
+const requestLoop = () => {
+  if (port.isOpen && consoleIsAwake) {
+    port.write("LOOP 1\n");
+  }
+};
+const wakeConsole = () => {
+  port.write("\n", err => console.error);
+};
 port.on("open", () => {
-  const requestLoop = () => port.write("LOOP 1\n");
-  // "wakes up" console
-  port.write("\n", err => {
-    if (err) {
-      return console.error("Error:", err.message);
-    }
-  });
-
+  wakeConsole();
   parser.on("data", data => {
+    // console.log("data ", data, data.length);
     // After recieving "wake up", console replies, and our parser transforms
     // the reply to "ACK". We can now send the "LOOP 1" command.
     if (data.toString() === "ACK") {
+      consoleIsAwake = true;
+      clearInterval(loopRequestInterval);
+      loopRequestInterval = setInterval(() => {
+        requestLoop();
+      }, 2000);
       return requestLoop();
     }
     const weatherStationData = jsonFromVantageLoopPacket(data.slice(1));
     process.stdout.write(
       JSON.stringify({ _id: new Date(), ...weatherStationData }) + "\n"
     );
-
-    setTimeout(() => {
-      requestLoop();
-    }, 2000);
   });
 });
