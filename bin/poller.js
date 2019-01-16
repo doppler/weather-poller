543ae06522c6f7e0c827844adcb3429c621efaf5
David Rose <doppler@gmail.com>
2019-01-16 07:23:11 -0500

    rearrange stuff to make bin executable

---
+++ bin/poller.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const weatherStationPoller = require("../weather-station-poller");
+
+weatherStationPoller();
---
+++ index.js
@@ -0,0 +1,3 @@
+const weatheStationPoller = require("./lib/poller");
+
+weatheStationPoller();
